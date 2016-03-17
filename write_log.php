0b7f0db78cd6f501a34e787e8cc5c9fd611aa5ec
Vikrant Ramteke <vikrantrc@gmail.com>
2016-03-17 13:47:01 +0530

    added demo write log file

---
+++ write_log.php
@@ -0,0 +1,48 @@
+<?php
+/*  echo "vik"; die;
+  $log  = "User: ".$_SERVER['REMOTE_ADDR'].' - '.date("F j, Y, g:i a").PHP_EOL.
+        "Attempt: ".($result[0]['success']=='1'?'Success':'Failed').PHP_EOL.
+        "User: ".$username.PHP_EOL.
+        "Pass: ".$password.PHP_EOL.
+        "-------------------------".PHP_EOL;
+//Save string to log, use FILE_APPEND to append.
+file_put_contents('./log_'.date("j.n.Y").'.txt', $log, FILE_APPEND);
+
+*/
+
+
+/**
+ * [set tracking code by log all data]
+ * @param  array  $error_log [description]
+ * @return [type]            [description]
+ */
+function put_data_file_log($error_log = array()) {
+  global $user, $base_url, $base_path;
+     
+  
+    $error_log['user_id'] = $user->uid;   
+    $border = PHP_EOL . str_repeat("+", 120) . PHP_EOL;
+    $error_log = $border . print_r($error_log, true) . $border;
+    $error_log['user_id'] = $user->uid;   
+    //Save string to log in drupal root, use FILE_APPEND to append.
+    file_put_contents(DRUPAL_ROOT . '/tmp/log_qiz_section'. date("Y-m-d--h-i") . '.txt', $error_log, FILE_APPEND);
+    //Uncomment below code to save string to your root dir, use FILE_APPEND to append.
+    //file_put_contents('./log_'.date("j.n.Y").'.txt', $log, FILE_APPEND);
+  return true;
+}
+
+$error_log = array(
+    'date' => date('Y-m-d h:m:s') ,
+    'file_name' => $_SERVER['SCRIPT_FILENAME'],
+    'ip_address' => $_SERVER['REMOTE_ADDR'] . ' - ' . date("F j, Y, g:i:s a") ,
+    'Message' => "Calling inside " . FUNCTION,
+    'method' => FUNCTION,
+    'session' => $_SESSION,
+    'url_access' => $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'],
+    'file_access' => FILE,
+    'url API' => $url,
+    'line_access' => LINE
+    );
+
+ //data tracking
+ put_data_file_log($error_log);
